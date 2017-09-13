67873772171d31dac352a6f8bec35d98b6d4b7a5
louis hellinger <louishellinger@louiss-MacBook-Pro.local>
2017-09-13 15:01:22 -0400

    first commit, initial file setup

---
+++ assets/script/app.js
@@ -0,0 +1,16 @@
+//INITIALIZE Firebase
+
+  var config = {
+    apiKey: "AIzaSyARhak_EXphrgvED2BpapqEvemFCP0171Q",
+    authDomain: "trainschedule-b9d74.firebaseapp.com",
+    databaseURL: "https://trainschedule-b9d74.firebaseio.com",
+    projectId: "trainschedule-b9d74",
+    storageBucket: "",
+    messagingSenderId: "711799850917"
+  };
+  firebase.initializeApp(config);
+
+
+ 
+
+
